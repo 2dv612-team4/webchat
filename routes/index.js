8b07ac0721873e679f71e7cf27676e16dd36635e
Eric <ej222pj@student.lnu.se>
2016-11-14 15:26:56 +0100

    Started connecting login to db

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const user = require('../model/DAL/userHandler.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {layout: 'index.hbs'});
+router.get('/', function (req, res, next) {
+  //TODO: Need redirect/sessions
+  res.render('index', { layout: 'index.hbs' });
+  console.log(user)
+});
+
+router.post('/', function (req, res) {
+  var userFromDb = user.findWithUsername(req.body.username);
+  console.log(userFromDb);
+  if (req.body.username == userFromDb.username && 
+      req.body.password == userFromDb.password) {
+    console.log("Rätt User")
+    //req.session.username = username;
+    //res.redirect('/redirect');
+  } else {
+    console.log("Fel User")
+    req.flash("info", "Wrong username or password!");
+    //res.redirect('/redirect');
+  }
 });
 
 module.exports = router;
