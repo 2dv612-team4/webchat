e856c1be9aa97925c61ecd86690cd47961d59195
rs222kn <rs222kn@student.lnu.se>
2016-11-15 14:12:50 +0100

    register sort of working..

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,29 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const userHandler = require('../model/DAL/userHandler.js');
+const co = require('co');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('register', {layout: 'register.hbs'});
+});
+
+/**
+ * 
+ */
+router.post('/', function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password; 
+  co(function*(){
+    const user = yield userHandler.findWithUsername(username); // SHOULD HASH AND SALT!!
+    if(user){
+      return res.redirect('/register', { message: 'Place holder massage'}); // TODO: enter massage
+    }
+    yield userHandler.add(username, password); // SHOULD HASH AND SALT!!
+    res.redirect('/', {message: 'Place holder massage'}); // TODO: enter massage
+  })
+  .catch(() => res.redirect('/register'));  
 });
 
 module.exports = router;
