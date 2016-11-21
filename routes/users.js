4eee84cb979d6ba3a1b1bac896a770fb3b9f641e
rs222kn <rs222kn@student.lnu.se>
2016-11-22 00:03:00 +0100

    Implemented user search by username

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,31 @@
 'use strict';
 const express = require('express');
+const userHandler = require('../model/DAL/userHandler.js');
+const co = require('co');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
+});
+
+router.get('/search/:username', function(req, res){
+  if(!req.session.loggedIn){
+    return res.sendStatus(401);
+  }
+  const username = req.params.username;
+  if(username === ''){
+    return res.sendStatus(406);
+  }
+  userHandler.findWithPartialUsername(username)
+    .then((users) => {
+      res.json(users.map((user) => {
+        return {
+          username: user.username,
+        };
+      }));
+    })
+    .catch(() => res.sendStatus(500));
 });
 
 module.exports = router;
