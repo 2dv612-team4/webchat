30c83c875f85e01ff0446c0e8d4c3ba4d8683ccb
rs222kn <rs222kn@student.lnu.se>
2016-11-22 13:52:16 +0100

    Get all friends implemented

--- client/webbchat/src/model/DAL/dbUser.js
+++ client/webbchat/src/model/DAL/dbUser.js
@@ -5,10 +5,8 @@ export const sendFriendRequestTo = (username) =>
   fetch(`${serverUrl}friends/sendrequest/${username}`,
     { method: 'POST' });
 
-
-export const getAllFriends = () => {
-
-};
+export const getAllFriends = () => 
+  fetch(`${serverUrl}friends/`);
 
 export const getAllUsersContaining = (query) => 
   fetch(`${serverUrl}users/search/${query}`);
