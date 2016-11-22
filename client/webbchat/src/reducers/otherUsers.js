8eeb3a1b9eb0960dd145a60c58864a63a0717101
rs222kn <rs222kn@student.lnu.se>
2016-11-22 13:53:55 +0100

    Removed implemented todo

--- client/webbchat/src/reducers/otherUsers.js
+++ client/webbchat/src/reducers/otherUsers.js
@@ -1,10 +1,5 @@
 
 const otherUsers = (state = [], action) => {
-  /**
-   * TODO: 
-   * - add usrs action,
-   * - remove users action 
-   * */  
   switch (action.type) {
   case 'SET_USERS':
     return action.users;    
