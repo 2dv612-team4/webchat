ef1f90483f1ef6d815e19f5e518c4dddd2e10934
rs222kn <rs222kn@student.lnu.se>
2016-11-28 13:37:24 +0100

    added remove friend function

--- client/webbchat/src/components/userList/friends/Users.js
+++ client/webbchat/src/components/userList/friends/Users.js
@@ -3,9 +3,13 @@ import { List } from 'react-mdl';
 import User from './User';
 
 class Users extends Component {
-  openChat(userId){
-    console.log('userId', userId);
+  openChat(username){
+    console.log('openChat for:', username);
     // TODO: open chat
+  }
+
+  removeFriend(username){
+    console.log('removeFriend:', username);
   }
 
   render() {
@@ -16,7 +20,8 @@ class Users extends Component {
               user.username.toLowerCase().match('^'+this.props.filterQuery.toLowerCase()+'.*'))
                 .map((user, i) => 
                   <User 
-                    openChat={this.openChat.bind(this)} 
+                    openChat={this.openChat.bind(this)}
+                    removeFriend={this.removeFriend.bind(this)} 
                     user={user} 
                     key={user.username}/>)}
       </List>
