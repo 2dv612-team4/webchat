437f17285b6942b3bb91d7cc68339aad64e4c2f2
rs222kn <rs222kn@student.lnu.se>
2016-11-28 13:35:11 +0100

    added menu on friend object with option to remove friend

--- client/webbchat/src/components/userList/friends/User.js
+++ client/webbchat/src/components/userList/friends/User.js
@@ -1,17 +1,32 @@
 import React, { PropTypes } from 'react';
-import { ListItem } from 'react-mdl';
+import { ListItem, IconButton, Menu, MenuItem } from 'react-mdl';
 
-const User = ({ openChat, user: {username} }) => (
-  <ListItem 
-    onClick={() => openChat(username)} icon="person">
-      {username}
+const User = ({ openChat, removeFriend, user: {username} }) => (
+  <ListItem icon="person"> 
+    <div
+      onClick={() => openChat(username)}>
+    {username}
+    </div>
+    <IconButton name="more_vert" id="demo-menu-lower-left" />
+    <Menu target="demo-menu-lower-left" >
+      <MenuItem     
+        onClick={() => openChat(username)}
+        >Open chat
+      </MenuItem>
+      <MenuItem 
+        onClick={() => removeFriend(username)}
+        >Remove friend
+      </MenuItem>
+    </Menu>
+    
+
   </ListItem>
 );
 
 User.propTypes = {
   user: PropTypes.object,
   openChat: PropTypes.func,
+  removeFriend: PropTypes.func,
 }
 
 export default User;
-
