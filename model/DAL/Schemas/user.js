a9c7bd4d2a538ab315c9d206dc44e729fc44bcd9
Kim <kn222gp@student.lnu.se>
2016-11-28 15:13:00 +0100

    Added payment date field in db.

--- model/DAL/Schemas/user.js
+++ model/DAL/Schemas/user.js
@@ -6,7 +6,8 @@ let Schema = mongoose.Schema;
 let userSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  socketId: { type: String },
+  socketId: {type: String},
+  premiumExpirationDate: {type: Date}, 
 
   friends: [
     { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
