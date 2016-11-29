bb2881714aced8d898ac9534014467e21d66c4e3
rs222kn <rs222kn@student.lnu.se>
2016-11-29 16:03:13 +0100

    removed pending, added fixedDrawer

--- client/webbchat/src/App.js
+++ client/webbchat/src/App.js
@@ -3,37 +3,27 @@ import React, { Component } from 'react';
 import Search from './components/search/App'
 import UserList from './components/userList/UserList'
 import Logout from './components/logout/Logout'
-import Pending from './components/pending/App'
+import { Layout, Header, Content, Drawer, Navigation} from 'react-mdl';
 import './App.css';
-import { Layout, Header, Content, Grid, Cell} from 'react-mdl';
 
 class App extends Component {
   
   render() {
     return (
       <div style={{height: '100vh', position: 'relative'}}>
-          <Layout fixedHeader>
+          <Layout fixedHeader fixedDrawer>
               <Header title="Webchat">
               <Logout />
               </Header>
+              <Drawer >
+              <Navigation>
+                <span><Search /></span>
+                <span><UserList /></span>
+              </Navigation>
+                
+              </Drawer>
               <Content>
-                <div style={{ margin: 'auto'}}>
-                  <Grid className="demo-grid-ruler" >
-                    <Cell col={2} style={{ minWidth: '200px'}} >
-                      <Search />
-                      <UserList />
-                    </Cell>
-                    <Cell col={10} style={{ margin: '2px'}}>CHAT</Cell>
-                  </Grid>
-                </div>
-                <div style={{ margin: 'auto'}}>
-                <Grid className="demo-grid-ruler" >
-                <Cell col={10} style={{ margin: '2px'}}>Requests</Cell>
-                  <Cell col={2} style={{ minWidth: '200px'}} >
-                    <Pending />
-                  </Cell>
-                </Grid>
-                </div>
+                chat
               </Content>
           </Layout>
       </div>
