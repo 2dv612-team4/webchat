e40abe13377550bed51ee22e45f75aa3af2542c9
rs222kn <rs222kn@student.lnu.se>
2016-11-30 23:58:27 +0100

    Moved css to be able to remove !important from css

--- client/webbchat/src/index.js
+++ client/webbchat/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { Provider } from 'react-redux';
 import store from './store';
 import socket from './model/DAL/socket/app';
+import './index.css';
 socket(store);
 
 
