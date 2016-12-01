918fc6eab4109369689d95f7d246bf62b1d6fd7d
kn222gp <kn222gp@student.lnu.se>
2016-12-01 13:57:42 +0100

    Working on the ad img.

--- client/webbchat/src/components/advertisements/Advertisement.js
+++ client/webbchat/src/components/advertisements/Advertisement.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
+import adImage from './advertisement.png';
 
 
 class Advertisement extends Component {
   render() {
     const isPremium = this.props.isPremium;
-    if(isPremium)
-    {
+    if(isPremium){
       return (<div></div>);
     }
 
     return ( 
       <div>
-        <img src='/images/advertisement.png' alt='pesky ad'
-        style={{
-          width: '400px',
-          height: '400px',
-          backgroundColor: 'blue',
-          border: '3px solid black',
-          zIndex: 9999,
-          display: 'block',
-        }}
-        />
-        <div style={{background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}></div>
-        <p>pelle gillar att se ads</p>
+        <img src={(adImage)} alt='pesky ad'/>
       </div>
     )
   }
