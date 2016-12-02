d1e197a3a80224e99d21d5cad1c1194286ef1167
rs222kn <rs222kn@student.lnu.se>
2016-12-02 08:35:44 +0100

    Removed fulhax

--- client/webbchat/src/components/advertisements/Advertisement.js
+++ client/webbchat/src/components/advertisements/Advertisement.js
@@ -1,15 +1,12 @@
 import React, { PropTypes } from 'react';
-import adImage from './advertisement.png';
 import connect from '../../connect/connect'
-
 const publicImage = '/images/advertisement.png';
-const isLocal = window.location.href.match('localhost:3000');
 
 const Advertisement = ({ isPremium }) => {
   if(isPremium){
     return null;
   }
-  return (<img src={ isLocal ? adImage : publicImage } alt='pesky ad'/>)
+  return (<img src={publicImage} alt='pesky ad'/>)
 };
 
 Advertisement.propTypes = {
