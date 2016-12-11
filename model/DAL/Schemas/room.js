6c66141bdbbbafc3b75337fcb04b1e3ffbddcdd7
gk222gs <gk222gs@student.lnu.se>
2016-12-11 15:45:11 -0800

    Added TTL to messages and refactored the room schema

--- model/DAL/Schemas/room.js
+++ model/DAL/Schemas/room.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
 let Schema = mongoose.Schema;
+
+let messageSchema = new Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true},
+  message: {type: String, required: true},
+  timestamp: {type: Date, default: Date.now, required: true},
+  createdAt: {type: Date, default: Date.now, expires: '30d'}
+});
+
 let roomSchema = new Schema({
   name: {type: String, required: true},
   users: [
     { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
   ],
-  messages: [
-    {
-      user: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true},
-      message: {type: String, required: true},
-      timestamp: {type: Date, default: Date.now, required: true},
-    },
-  ],
+  messages: [messageSchema],
   timestamp: {type: Date, default: Date.now, required: true},
 });
 
