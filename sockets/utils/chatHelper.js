8edffd6433c70e56f4d27f1083dba35bde680313
rs222kn <rs222kn@student.lnu.se>
2016-12-14 12:54:01 +0100

    added helper to leave chat and make new from friend chat

--- sockets/utils/chatHelper.js
+++ sockets/utils/chatHelper.js
@@ -8,16 +8,44 @@ const addMessageToRoom =
     return yield roomHandler.addMessage(roomId, user._id, message);
   });
 
-
 const removeAllMessagesFromChatRoom = 
   co.wrap(function*(chatId){
     yield roomHandler.removeAllMessages(chatId);
     const chat = yield roomHandler.findRoomWithId(chatId);
     return chat.name;
+  });
 
-  })
+const createNewGroupChatFromFriendChat = 
+  co.wrap(function*(friendChatId, usersToAdd){
+    const { users, messages } = yield roomHandler.findRoomWithId(friendChatId); // old chat
+
+    const usersObjects = yield Promise.all(users.map(user => userHandler.findWithId(user))); // all users in old chat
+
+    const chatParticipants = usersObjects
+      .map(({username}) => username)
+      .join(', ');
+
+    const groupChat = yield roomHandler.add(`Chat with ${chatParticipants}`, true);
+
+    yield Promise.all(users.map(user => roomHandler.addUser(groupChat._id, user._id)));
+    yield Promise.all(usersToAdd.map(user => roomHandler.addUser(groupChat._id, user)));
+    yield Promise.all(usersObjects.map(user => roomHandler.addUser(groupChat._id, user)));
+    
+    yield roomHandler.addMessages(groupChat._id, messages);
+
+    return roomHandler.findRoomWithIdAndPopulateAll(groupChat._id);
+  });
+
+const leavGroupChat = 
+  co.wrap(function*(username, chatId){
+    const user = yield userHandler.findWithUsername(username);
+    yield roomHandler.leaveChat(chatId, user._id);
+    return roomHandler.findRoomWithIdAndPopulateAll(chatId);
+  });
 
 module.exports = {
   addMessageToRoom,
   removeAllMessagesFromChatRoom,
+  createNewGroupChatFromFriendChat,
+  leavGroupChat,
 };
