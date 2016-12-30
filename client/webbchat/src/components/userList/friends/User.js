1683afd37e1189002c354376385f095d1289238d
Martin Arvidsson <Martinarvidsson1994@gmail.com>
2016-12-30 15:28:57 +0100

    Fixed report user window

--- client/webbchat/src/components/userList/friends/User.js
+++ client/webbchat/src/components/userList/friends/User.js
@@ -2,27 +2,27 @@ import React, { PropTypes } from 'react';
 import { ListItem, IconButton, Menu, MenuItem, ListItemAction, ListItemContent } from 'react-mdl';
 
 const User = ({ openChat, removeFriend, clearChatHistory, user: {username}, chat: {_id: chatId}, reportUser }) => (
-  <ListItem icon="person" > 
+  <ListItem icon="person" >
     <ListItemContent
       onClick={() => openChat(chatId)}>{username}</ListItemContent>
     <ListItemAction>
       <IconButton name="more_vert" id={`menu_iconbutton_id_${username}`}/>
       <Menu align="right" target={`menu_iconbutton_id_${username}`}>
-        <MenuItem     
+        <MenuItem
           onClick={() => openChat(chatId)}>Open chat
         </MenuItem>
-        <MenuItem 
+        <MenuItem
           onClick={() => removeFriend(username, chatId)}>Remove friend
         </MenuItem>
-        <MenuItem 
+        <MenuItem
           onClick={() => clearChatHistory(chatId)}>Clear chat history
         </MenuItem>
-        <MenuItem 
+        <MenuItem
           onClick={() => reportUser(username)}>Report user
         </MenuItem>
       </Menu>
     </ListItemAction>
-    
+
   </ListItem>
 );
 
