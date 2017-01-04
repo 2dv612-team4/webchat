5d28fcf3946afc11219fcad5ea67bd8a1c1aeeff
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2017-01-04 15:32:02 +0100

    Changed authenticate

--- routes/utils/authenticate.js
+++ routes/utils/authenticate.js
@@ -2,7 +2,7 @@
 const authenticate = (req, res, next) => {
   // For development purpose only
   if(process.argv.find(a => a === 'development')){
-    req.session.loggedIn = 'user2';
+    req.session.loggedIn = 'dev';
     return next();
   }
 
